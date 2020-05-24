cb5854de754bcc5774c9cb9a4e2a8870005e5269
MOHAMED-ENSA2 <65856387+MOHAMED-ENSA2@users.noreply.github.com>
2020-05-24 22:32:45 +0100

    create class.py
    create class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+# create a class 
+class student : 
+   name
+   def __init__(self,name) : 
+      self.name = name
+   def disp(self) : 
+      print(self.name)
+ 
+Mohamed = student("Mohamed")
+Mohamed.disp()
